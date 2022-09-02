52036467212ee5c66962cce3a797ecf7c6ebb5db
dimaskest <104227196+dimaskest@users.noreply.github.com>
2022-09-02 17:07:52 +0300

    delete task2.py

--- task2.py
+++
@@ -1,9 +0,0 @@
-# I like this language very much!
-print("I like this language very much!")
-
-# I like this language very much!
-print("I like this language very much", end="!")
-
-# I like this language very much!
-print("\nI like this language ", "much!", sep="very ")
-
