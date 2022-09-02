29c8d8d81f5a3cd64c2f1e8f98ab5737feb0eb90
dimaskest <104227196+dimaskest@users.noreply.github.com>
2022-09-02 17:12:06 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,14 @@
+# 1st variant
+print("######### \n#\t#\n#\t#\n#\t# \n#########")
+print("\n#\t# \n#\t# \n######### \n#\t# \n#\t#\n")
+
+# 2nd variant
+print("#"*9, "\n#\t#"*3, "\n#########")
+print("\n", "\n#\t#"*2, "\n#########", "\n#\t#"*2)
+
+# 3rd variant
+a = "\n#########"
+print(a, "\n#\t#"*3, a)
+
+b = "\n#\t#"*2
+print(b, a, b)
