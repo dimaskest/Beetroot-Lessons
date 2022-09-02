ff829550368b800f1b97cb6fa46fd9daffb9d4ec
dimaskest <104227196+dimaskest@users.noreply.github.com>
2022-09-02 16:57:24 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+# I like this language very much!
+print("I like this language very much!")
+
+# I like this language very much!
+print("I like this language very much", end="!")
+
+# I like this language very much!
+print("\nI like this language ", "much!", sep="very ")
+
