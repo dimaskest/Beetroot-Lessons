aa5b3340695430fb16d5c5db070d25b65b474b7f
dimaskest <104227196+dimaskest@users.noreply.github.com>
2022-09-09 15:43:35 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -2,6 +2,10 @@ import random
 
 
 string = input("Enter your word: ")
-mixed_string = "".join(random.sample(string, len (string)))
+mixed_string_1 = "".join(random.sample(string, len (string)))
+mixed_string_2 = "".join(random.sample(string, len (string)))
+mixed_string_3 = "".join(random.sample(string, len (string)))
+mixed_string_4 = "".join(random.sample(string, len (string)))
+mixed_string_5 = "".join(random.sample(string, len (string)))
 
-print(f"'{mixed_string}', " * 5)
+print(mixed_string_1, mixed_string_2, mixed_string_3, mixed_string_4, mixed_string_5)
